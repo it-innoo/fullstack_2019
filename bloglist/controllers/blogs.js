dc1ddf262f90a687987c98deeaa8b51a028b628c
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-11-20 19:18:23 +0200

    4.10: blogilistan testit, step3

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -6,14 +6,23 @@ router.get('/', async (_request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-router.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+router.post('/', async (request, response, next) => {
+  const body = request.body
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  })
+
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog.toJSON())
+  } catch (error) {
+    next(error)
+  }
+
 })
 
 module.exports = router
--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -1,27 +1,19 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0,
-  }
-]
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
 })
 
 
@@ -35,7 +27,39 @@ test('blogs are returned as json', async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body.length).toBe(initialBlogs.length)
+  expect(response.body.length).toBe(helper.initialBlogs.length)
+})
+
+test('identier is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  const ids = response.body.map(r => r.id)
+  expect(ids).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 0,
+  }
+
+  console.log(`initial blogs ${helper.initialBlogs.length}`)
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+
+  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+
+  const title = blogsAtEnd.map(r => r.title)
+  expect(title)
+    .toContain('Go To Statement Considered Harmful')
 })
 
 afterAll(() => {
