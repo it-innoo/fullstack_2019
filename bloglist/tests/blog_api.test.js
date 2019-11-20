0beadbf6913510a17bd3823219f44d1e5b17f88a
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-11-20 16:53:04 +0200

    4.8: blogilistan testit, step 1

---
+++ bloglist/tests/blog_api.test.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+})
+
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body.length).toBe(initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
--- bloglist/utils/config.js
+++ bloglist/utils/config.js
@@ -2,6 +2,15 @@ require('dotenv').config()
 
 let { PORT, MONGODB_URI } = process.env
 
+console.log('using environment: ', process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+  MONGODB_URI = process.env.MONGODB_DEV_URI
+}
+
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.MONGODB_TEST_URI
+}
+
 module.exports = {
   MONGODB_URI,
   PORT
